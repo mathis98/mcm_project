1a3adbb61f68dabf9b0ec4c4fd4233cf9b006b0e
cdonat2 <you@example.com>
2021-07-13 12:50:03 +0100

    Update metropolis_hastings.py

--- examples/metropolis_hastings.py
+++ examples/metropolis_hastings.py
@@ -1,42 +1,204 @@
 """
-Example for Metropolis Hastings
+Metropolis Hastings Sampling
 """
+
+# TODO: Add 2d Case
+# TODO: Add Hastings
 
 # Imports
 import numpy as np
 import scipy.stats as st
 import seaborn as sns
-from matplotlib import pyplot as plt
-
-def circle(x, y):
-    return (x-1)**2 + (y-2)**2 - 3**2
-
-
-def pgauss(x, y):
-    return st.multivariate_normal.pdf([x, y], mean=mus, cov=sigmas)
+import matplotlib.pyplot as plt
+import logging
+import time
+import statistics
+import math
 
 
-def metropolis_hastings(p, iter=1000):
-    x, y = 0., 0.
-    samples = np.zeros((iter, 2))
+class MetropolisHastingsSampler:
+    def __init__(self, mu, sigma, x_range, step_size, seed):
+        """
+        Slice Sampler
+        :param mu:
+        :param sigma:
+        :param x_range:
+        :param w_length:
+        :param seed:
+        """
+        logging.basicConfig(level=logging.INFO)
+        self.mu = mu
+        self.sigma = sigma
+        self.x_range = x_range
+        self.step_size = step_size
+        np.random.seed(seed)
+        sns.set()
 
-    for i in range(iter):
-        x_star, y_star = np.array([x, y]) + np.random.normal(size=2)
-        if np.random.rand() < p(x_star, y_star) / p(x, y):
-            x, y = x_star, y_star
-        samples[i] = np.array([x, y])
+    def p(self, x, mu, sigma):
+        """
+        Defines a 1D Target Distribution
+        :param x: x value
+        :param mu: List of Expected Values
+        :param sigma: List of Standard Deviations
+        :return:
+        """
+        p = np.zeros(len(mu))
+        for idx, mu_val in enumerate(mu):
+            sigma_val = sigma[idx]
+            p[idx] = st.norm.pdf(x, loc=mu_val, scale=sigma_val)
+        return sum(p)
 
-    return samples
+    def sample(self, x_prior, y_prior):
+        """
+        Creates a single Sample. x_value is the prior x
+        :param x_value: float
+        :param y_value: float
+        :return: array sample
+        """
+        sample = [0, 0]
+        logging.debug('Prior Sample:{}{}'.format(x_prior, y_prior))
 
-mus = np.array([5, 5])
-sigmas = np.array([[1, .9], [.9, 1]])
+        # Propose Sample
+        x_proposed = np.random.normal(x_prior, 3, 1)  # This is symmetric, so metropolis
+        y_proposed = 0.
+        logging.debug('Proposed Sample:{}{}'.format(x_proposed, y_proposed))
 
-samples = metropolis_hastings(circle, iter=10000)
-sns.jointplot(samples[:, 0], samples[:, 1])
+        # Accept / Reject
+        p_prior = sampler.p(x_prior, self.mu, self.sigma)
+        logging.debug(('Probability Prior {}'.format(p_prior)))
+        p_proposed = sampler.p(x_proposed, self.mu, self.sigma)
+        logging.debug(('Probability Proposed {}'.format(p_proposed)))
+
+        g_ab = 1. # TODO: Adjust here for Hastings
+        g_ba = 1.
+        rf = p_proposed/p_prior
+        rg = g_ab/g_ba
+
+        acceptance_ratio = rf * rg
+        acceptance_probability = min(1, acceptance_ratio)
+        logging.debug('Acceptance Probability: {}'.format(acceptance_probability))
+
+
+        if np.random.rand() < acceptance_probability:
+            accept = True
+            logging.debug('Sample Accepted: [{} {}]'.format(x_proposed, y_proposed))
+            sample[0] = x_proposed
+            sample[1] = y_proposed
+        else:
+            accept = False
+            logging.debug('Sample Rejected')
+
+        return sample, accept
+
+
+    def sample_1d(self, samples_n, burnin, plot = False):
+        """
+        Creates n samples in 1d
+        :return: sample
+        """
+
+        # Create Target Distribution
+        x_vector = np.linspace(self.x_range[0], self.x_range[1], 1000)
+        target_distribution = np.zeros(len(x_vector))
+        for idx, val in enumerate(x_vector):
+            target_distribution[idx] = sampler.p(val, self.mu, self.sigma)
+
+        # Create Samples
+        samples = np.empty([samples_n, 2])
+
+        # Set up Plotting
+        if plot:
+            plt.ion()
+            figure, ax = plt.subplots(figsize=(10, 8))
+            line_distribution, = ax.plot(x_vector, target_distribution, '-', linewidth=2, markersize=1)
+            marker_x_value, = ax.plot(0, 0, 'xr', linewidth=2, markersize=10)
+            marker_y_value, = ax.plot(0, 0, 'xy', linewidth=1, markersize=10)
+            marker_samples, = ax.plot(samples[:, 0], samples[:, 1], 'xb', linewidth=2, markersize=10)
+            plt.title("Metropolis Hastings Sampling", fontsize=16)
+            plt.xlim(x_range)
+            plt.ylim([-0.1, 0.75])
+            plt.xlabel("X")
+            plt.ylabel("Y")
+
+        # Create Samples
+        x_value = np.random.uniform(self.x_range[0], self.x_range[1])  # Random x (Slice) for first sample
+        y_value = 0.                                                   # y for first sample
+        for i in range(samples_n):
+            sample, accept = sampler.sample(x_value, y_value)
+            samples[i, 0] = sample[0]
+            samples[i, 1] = sample[1]
+
+            # Update Plot
+            if plot:
+                # updating data values
+                timer = 0.0001
+                y_value = sample[1]
+                line_distribution.set_xdata(x_vector)
+                line_distribution.set_ydata(target_distribution)
+                figure.canvas.flush_events()
+                time.sleep(timer)
+                marker_x_value.set_xdata(x_value)
+                marker_x_value.set_ydata(0)
+                figure.canvas.flush_events()
+                time.sleep(timer)
+                marker_y_value.set_xdata(x_value)
+                marker_y_value.set_ydata(y_value)
+                figure.canvas.flush_events()
+                time.sleep(timer)
+                marker_samples.set_xdata(samples[:, 0])
+                marker_samples.set_ydata(samples[:, 1])
+                figure.canvas.flush_events()
+                time.sleep(timer)
+                figure.canvas.draw() # drawing updated values
+
+                # This will run the GUI event loop until all UI events currently waiting have been processed
+                figure.canvas.flush_events()
+                time.sleep(timer)
+
+            if accept:
+                x_value = sample[0]
+                y_value = sample[1]
+
+        return samples
+
+samples_n = 1000
+burnin = 200
+mu = [15, 20]
+sigma = [1, 3]
+x_range = [5, 35]
+w_length = 0.5
+seed = 0
+
+sampler = MetropolisHastingsSampler(mu, sigma, x_range, w_length, seed)
+samples = sampler.sample_1d(samples_n, burnin, plot=False)
+samples_accepted = samples[~np.all(samples == 0, axis=1)]
+
+# Plot End Result
+x = np.linspace(x_range[0], x_range[1], 1000)
+target_distribution = np.zeros(len(x))
+for idx, val in enumerate(x):
+    target_distribution[idx] = sampler.p(x[idx], mu, sigma)
+
+target_distribution_norm = [float(i)/sum(target_distribution) for i in target_distribution]
+
+plt.figure(figsize=(17, 6))
+plt.hist(samples_accepted[:, 0], bins=30, color='b', density=True, alpha=0.6)
+plt.plot(x, target_distribution, '-r', linewidth=2)
+plt.title("Metropolis Hastings Sampling", fontsize=16)
+plt.xlim(x_range)
+plt.ylim([-0.1, 0.75])
+plt.xlabel("X")
+plt.ylabel("Y")
 plt.show()
 
-samples = metropolis_hastings(pgauss, iter=10000)
-sns.jointplot(samples[:, 0], samples[:, 1])
-plt.show()
+# Momentums
+samples_mean = statistics.mean(samples_accepted[:, 0])
+samples_std = statistics.stdev(samples_accepted[:, 0])
+target_mean = sum(np.multiply(x, target_distribution_norm))
+target_std = math.sqrt(sum(np.multiply(target_distribution_norm, (x-target_mean)**2)))
 
-__source__ = 'https://wiseodd.github.io/techblog/2015/10/17/metropolis-hastings/'
+print('Mean of Samples: {}'.format(samples_mean))
+print('Mean of Target: {}'.format(target_mean))
+print('Standard Deviation of Samples: {}'.format(samples_std))
+print('Standard Deviation of Target: {}'.format(target_std))
+print('Standard Error: {}'.format((samples_std/samples_n)**0.5))
